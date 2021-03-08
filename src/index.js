747a49db6e9e188dd877466bbd5effab8dc6412d
ddagerr <gershanok32@gmail.com>
2021-03-08 15:50:46 +0300

    feat: create reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+        if (n<0) n=-n;
+        n=String(n)
+        n=n.split('')
+        let reverseN=[]
+        for(let i=0;i<n.length;i++){
+        reverseN[i]=n[n.length-i-1]
+        }
+        return Number(reverseN.join(''))
 }
